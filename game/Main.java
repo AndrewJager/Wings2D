8c80b2ea31693ffc645e6d9adb3cc91d00fc712f
Drew J <35353143+CatsAreEvil@users.noreply.github.com>
2020-01-04 21:37:01 -0500

    Create window for graphics settings

--- game/Main.java
+++ game/Main.java
@@ -1,15 +1,21 @@
 package game;
 import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GraphicsConfiguration;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.awt.GridBagLayout;
 import java.awt.Point;
 import java.awt.Shape;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
@@ -17,6 +23,12 @@ import java.awt.event.MouseEvent;
 import java.awt.image.BufferStrategy;
 import java.awt.image.VolatileImage;
 
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import framework.*;
 
@@ -32,6 +44,8 @@ public class Main extends Canvas implements Runnable
 	private Thread thread;
 	private boolean running = false;
 	private boolean mouseDown = false;
+	
+	private boolean initReady = false;
 	
 	private LevelManager manager;
 	private KeyState keys;
@@ -103,8 +117,88 @@ public class Main extends Canvas implements Runnable
 		strat.show();
 	}
 	
+	private void setupGrapics() 
+	{
+		JFrame frame = new JFrame("test");
+		int localWidth = 800;
+		int localHeight = 700;
+		frame.setPreferredSize(new Dimension(localWidth, localHeight));
+		frame.setMaximumSize(new Dimension(localWidth + 50, localHeight + 50));
+		frame.setMinimumSize(new Dimension(localWidth, localHeight));
+		
+		JPanel panel = new JPanel();
+		panel.setBounds((int)(frame.getWidth() * 0.05), (int)(frame.getHeight() * 0.02), (int)(frame.getWidth() * 0.9), (int)(frame.getHeight() * 0.9));
+		panel.setBackground(Color.LIGHT_GRAY);
+
+		JButton readyButton = new JButton("Begin!");
+		readyButton.setLocation(new Point(150, (int)(panel.getHeight() * 0.90)));
+		readyButton.setSize(80, 25);
+	    readyButton.addActionListener(new ActionListener(){  
+	    	public void actionPerformed(ActionEvent e){  
+	    	            initReady = true;
+	    	        }  
+	    	    });  
+		JCheckBox isFullscreen = new JCheckBox("Fullscreen");
+		isFullscreen.setBounds(25, 30, 100, 25);
+		JLabel sizingText = new JLabel("Change the size of the screen to the desired size.");
+		sizingText.setHorizontalAlignment(JLabel.CENTER);
+		sizingText.setBounds(25,  70,  400, 25);
+		JLabel sizingText2 = new JLabel("You will not be able to change it while the game is running.");
+		sizingText2.setHorizontalAlignment(JLabel.CENTER);
+		sizingText2.setBounds(25,  100, 400, 25);
+		JTextField inputWidth = new JTextField();
+		inputWidth.setBounds(25, 130, 50, 25);
+		JTextField inputHeight = new JTextField();
+		inputHeight.setBounds(75, 160, 50, 25);
+		
+		frame.addComponentListener(new ComponentAdapter() 
+		{  
+		        public void componentResized(ComponentEvent evt) {
+		            Component c = (Component)evt.getSource();
+		            inputWidth.setText(Integer.toString(c.getWidth()));
+		            inputHeight.setText(Integer.toString(c.getHeight()));
+		            
+		            panel.setBounds((int)(frame.getWidth() * 0.05), (int)(frame.getHeight() * 0.02), (int)(frame.getWidth() * 0.9), (int)(frame.getHeight() * 0.9));
+		            isFullscreen.setLocation((panel.getWidth() / 2) - (isFullscreen.getWidth() / 2), isFullscreen.getY());
+		            sizingText.setLocation((panel.getWidth() / 2) - (sizingText.getWidth() / 2), sizingText.getY());
+		            sizingText2.setLocation((panel.getWidth() / 2) - (sizingText2.getWidth() / 2), sizingText2.getY());
+		            inputWidth.setLocation((panel.getWidth() / 2) - (inputWidth.getWidth() / 2), inputWidth.getY());
+		            inputHeight.setLocation((panel.getWidth() / 2) - (inputHeight.getWidth() / 2), inputHeight.getY());
+		            readyButton.setLocation((panel.getWidth() / 2) - (readyButton.getWidth() / 2), (int)(panel.getHeight() * 0.90));
+		        }
+		});
+		frame.getContentPane().setLayout(null);
+		panel.setLayout(null);
+		frame.add(panel);
+		panel.add(readyButton);
+		panel.add(isFullscreen);
+		panel.add(sizingText);
+		panel.add(sizingText2);
+		panel.add(inputWidth);
+		panel.add(inputHeight);
+		
+		frame.setVisible(true);
+		
+		
+		while (!initReady)
+		{
+			try //Don't run as fast as possible
+			{
+			    Thread.sleep(50);
+			}
+			catch(InterruptedException ex)
+			{
+			    Thread.currentThread().interrupt();
+			}
+		}
+		
+		frame.setVisible(false);
+		frame.dispose();
+	}
+	
 	public Main()
 	{
+		setupGrapics();
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		scale = screenSize.getWidth() / WIDTH;
 		scale = 1.0;
