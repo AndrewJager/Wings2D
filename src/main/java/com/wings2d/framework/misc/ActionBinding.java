6a0ce4ce634a8f4e37856dd8da4672ea5abe82bc
CatsAreEvil <35353143+CatsAreEvil@users.noreply.github.com>
2022-02-13 14:36:42 -0500

    Add keybind classes

---
+++ src/main/java/com/wings2d/framework/misc/ActionBinding.java
@@ -0,0 +1,38 @@
+package com.wings2d.framework.misc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ActionBinding {
+	private List<KeyBind> bindings;
+	
+	public ActionBinding() {
+		bindings = new ArrayList<KeyBind>();
+	}
+	public ActionBinding(final String keys) {
+		this();
+		String[] keyStr = keys.split("\\+");
+		for (int i = 0; i < keyStr.length; i++) {
+			bindings.add(new KeyBind(keyStr[i].trim()));
+		}
+	}
+	
+	public void addKey(final KeyBind key) {
+		bindings.add(key);
+	}
+	
+	public String getValue() {
+		String str = "";
+		for (int i = 0; i < bindings.size(); i++) {
+			if (!str.equals("")) {
+				str = str + " + ";
+			}
+			str = str + bindings.get(i).getValue();
+		}
+		return str;
+	}
+	
+	public List<KeyBind> getKeys() {
+		return bindings;
+	}
+}
---
+++ src/main/java/com/wings2d/framework/misc/KeyBind.java
@@ -0,0 +1,13 @@
+package com.wings2d.framework.misc;
+
+public class KeyBind{	
+	private String key;
+	
+	public KeyBind(final String key) {
+		this.key = key;
+	}
+	
+	public String getValue() {
+		return key;
+	}
+}
